5d7683d2c7c6029d8273b771f2156f9f17473c3f
Kisuk Jeong <kisuk.jeong.lds@gmail.com>
2023-01-31 00:41:56 +0900

    메인화면 하단 네비게이션 버튼 준비

---
+++ android/app/src/main/java/io/github/jeongks/idealrpg/GameHomeActivity.java
@@ -0,0 +1,14 @@
+package io.github.jeongks.idealrpg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_home);
+    }
+}
