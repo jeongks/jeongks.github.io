bfac16d237bb40dc70bdf3b162d3bc4c95402af7
Kisuk Jeong <kisuk.jeong.lds@gmail.com>
2023-05-11 23:42:11 +0900

    update page

---
+++ backend/DietChecker/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+module.exports = (async function(){
+    const app = express();
+    
+    app.use()
+
+    return app;
+})();
---
+++ backend/DietChecker/index.js
@@ -0,0 +1,7 @@
+const app = require('./app');
+
+(async function(){
+    app.listen('8080', function(req, res){
+        console.log()
+    })
+})
